086e84268e8cd175a0127bd22396d6b29dfa0adc
AnasNaserEddin <34215731+AnasNaserEddin@users.noreply.github.com>
2020-10-26 09:45:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-#!/usr/local/bin/python3
-#
-# basic_flask.py
-# class_6
+# This is a sample Python script.
 
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import flask
-import random
+app = flask.Flask("__main__",template_folder='template')
+print(app)
 
-app = flask.Flask("MyApp") # Creating a controller (the object that organizes everything)
 
-@app.route('/roll/<int:number>')
-def index(number):
-    if(number not in range(1,101)):
-        return "please roll a number between 1 and 100"
-    else:
-        rand_num=random.randint(1, 100)
-        if(rand_num==number):
-            return ("success")
-        else:
-            return ("failed ")
-
+@app.route("/")
+@app.route("/cv")
+def index():
+    return flask.render_template("cv.html",title="My cv")
 
 app.run(debug=True)
+
+
+
